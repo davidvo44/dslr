011efe0bd72da436dfb8db8a4c31e452e24c131a
dvo <dvo@student.42lyon.fr>
2026-03-10 16:50:30 +0100

    logreg predict, rechange formula, didn't work well

--- logreg_predict.py
+++ logreg_predict.py
@@ -1,6 +1,7 @@
 import sys
 from utils import checkFile_csv
 import os,stat 
+import click
 import pandas as pd
 import math
 from logreg_train import ft_exp
@@ -19,45 +20,52 @@ def houseStatInterface():
 def main(file_path):
     os.chmod("db.csv", stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH);
     inputFile = pd.read_csv("dataset_train.csv");
-    dataFile = pd.read_csv("db.csv");
+    dataFile = pd.read_csv("db.csv").set_index("House");
     for i in range(len(inputFile)):
         firstName = inputFile["First Name"].iloc[i];
         lastName = inputFile["Last Name"].iloc[i];
         houseResult = predictmodel(dataFile, inputFile, i);
         houseResult =  str(inputFile["Index"].iloc[i]) + "," + houseResult;
         print(houseResult);
+        if i == 2:
+            return;
     return;
 
 
-def predictmodel(dataFile, inputFile, i):
+def predictmodel(dataFile, inputFile, indexStudent):
     result = houseStatInterface();
     count = 0;
     subjectList = ["Arithmancy","Astronomy","Herbology","Defense Against the Dark Arts","Divination","Muggle Studies","Ancient Runes","History of Magic","Transfiguration","Potions","Care of Magical Creatures","Charms","Flying"]
     houseList = ["Ravenclaw", "Gryffindor", "Slytherin", "Hufflepuff"];
-    for i in  range (12):
-        subject = subjectList[i];
-        bias = dataFile[subject].iloc[0];
-        if bias == 0:
-            continue;
+    
+    for subject in  subjectList:
         count += 1;
-        for iHouse in range (3):
-            indexWeight =  dataFile[dataFile["House"] == houseList[iHouse]].index[0];
-            result[houseList[iHouse]]["value"] = dataFile[subject].iloc[indexWeight];
+        for idxHouse in houseList:
+            bias = dataFile.loc[idxHouse, 'Bias'];
+            weight =  dataFile.loc[idxHouse, subject];
+            if weight == 0:
+                continue;
+            studentScore = inputFile[subject].iloc[indexStudent];
+            result[idxHouse]["value"] += studentScore * weight;
+            click.echo(click.style(f"\nDEBUG MODE: {result[idxHouse]['value']}", fg='cyan'));
     if count != 0:
-        for iHouse in range (3):
-            result[houseList[iHouse]]["value"] /= count;
+        for idxHouse in houseList:
+            bias = dataFile.loc[idxHouse, 'Bias'];
+            if bias == 0:
+                continue;
+            print (result[idxHouse]["value"])
+            # result[idxHouse]["value"] = sigmoidFormula([idxHouse]["value"]);
     houseResult = "Ravenclaw";
-    for iHouse in range (3):
-        if result[houseList[iHouse]]["value"] > result[houseResult]["value"]:
-            houseResult = houseList[iHouse];
+    for idxHouse in houseList:
+        if result[idxHouse]["value"] > result[houseResult]["value"]:
+            houseResult = idxHouse;
     return houseResult;
                 
                 
             
 
 
-def sigmoidFormula(weight, bias, studentScore):
-    z = weight * studentScore + bias;
+def sigmoidFormula(z):
     return 1.0 / (1.0 + ft_exp(-z))
 
 
@@ -70,4 +78,3 @@ if __name__ == '__main__':
             main(file_path);
         else:
             print("Invalid File");
-
