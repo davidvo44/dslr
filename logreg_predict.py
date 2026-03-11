fafc461e494ee4f46e92e08e761a74546b1f3a38
Alban Favier <afavier@z4r4p3.42lyon.fr>
2026-03-11 12:46:50 +0100

    correct nan in predict and start normalisation to train

--- logreg_predict.py
+++ logreg_predict.py
@@ -14,52 +14,55 @@ def houseStatInterface():
     "Gryffindor":  {"index": "Gryffindor", "value": 0},
     "Hufflepuff":  {"index": "Hufflepuff", "value": 0},
     }
-    return result;
+    return result
 
 
 def main(file_path):
-    os.chmod("db.csv", stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH);
-    inputFile = pd.read_csv("dataset_train.csv");
-    dataFile = pd.read_csv("db.csv").set_index("House");
+    os.chmod("db.csv", stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
+    inputFile = pd.read_csv(file_path)
+    dataFile = pd.read_csv("db.csv").set_index("House")
     for i in range(len(inputFile)):
-        firstName = inputFile["First Name"].iloc[i];
-        lastName = inputFile["Last Name"].iloc[i];
-        houseResult = predictmodel(dataFile, inputFile, i);
-        houseResult =  str(inputFile["Index"].iloc[i]) + "," + houseResult;
-        print(houseResult);
-        if i == 2:
-            return;
-    return;
+        firstName = inputFile["First Name"].iloc[i]
+        lastName = inputFile["Last Name"].iloc[i]
+        houseResult = predictmodel(dataFile, inputFile, i)
+        houseResult =  str(inputFile["Index"].iloc[i]) + "," + houseResult
+        print(houseResult)
+#        if i == 2:
+#           return
+    return
 
 
 def predictmodel(dataFile, inputFile, indexStudent):
-    result = houseStatInterface();
-    count = 0;
+    result = houseStatInterface()
+    count = 0
     subjectList = ["Arithmancy","Astronomy","Herbology","Defense Against the Dark Arts","Divination","Muggle Studies","Ancient Runes","History of Magic","Transfiguration","Potions","Care of Magical Creatures","Charms","Flying"]
-    houseList = ["Ravenclaw", "Gryffindor", "Slytherin", "Hufflepuff"];
+    houseList = ["Ravenclaw", "Gryffindor", "Slytherin", "Hufflepuff"]
     
     for subject in  subjectList:
-        count += 1;
+        count += 1
         for idxHouse in houseList:
-            bias = dataFile.loc[idxHouse, 'Bias'];
-            weight =  dataFile.loc[idxHouse, subject];
+            bias = dataFile.loc[idxHouse, 'Bias']
+            weight =  dataFile.loc[idxHouse, subject]
             if weight == 0:
-                continue;
-            studentScore = inputFile[subject].iloc[indexStudent];
-            result[idxHouse]["value"] += studentScore * weight;
-            click.echo(click.style(f"\nDEBUG MODE: {result[idxHouse]['value']}", fg='cyan'));
+                continue
+            studentScore = inputFile[subject].iloc[indexStudent]
+            if pd.isna(studentScore):
+                continue
+            result[idxHouse]["value"] += studentScore * weight
+            #click.echo(click.style(f"\nDEBUG MODE: {result[idxHouse]['value']}", fg='cyan'))
     if count != 0:
         for idxHouse in houseList:
-            bias = dataFile.loc[idxHouse, 'Bias'];
+            bias = dataFile.loc[idxHouse, 'Bias']
             if bias == 0:
-                continue;
-            print (result[idxHouse]["value"])
-            # result[idxHouse]["value"] = sigmoidFormula([idxHouse]["value"]);
-    houseResult = "Ravenclaw";
+                continue
+            #print (result[idxHouse]["value"])
+            result[idxHouse]["value"] += bias
+            result[idxHouse]["value"] = sigmoidFormula(result[idxHouse]["value"])
+    houseResult = "Ravenclaw"
     for idxHouse in houseList:
         if result[idxHouse]["value"] > result[houseResult]["value"]:
-            houseResult = idxHouse;
-    return houseResult;
+            houseResult = idxHouse
+    return houseResult
                 
                 
             
@@ -71,10 +74,10 @@ def sigmoidFormula(z):
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("error argument file");
+        print("error argument file")
     else:
         file_path = "datasets/dataset_test.csv"
         if (checkFile_csv(sys.argv[1])== True):
-            main(file_path);
+            main(sys.argv[1])
         else:
-            print("Invalid File");
+            print("Invalid File")
